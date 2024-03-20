9bcb638683673d9f6a47a1e41a11eb6522d14121
Peyal Dhar <pd185197@ncr.com>
2024-03-20 14:21:56 -0400

    Added deep link functionalities.

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { Text, Button, View, HStack } from "native-base";
-import { NativeModules } from "react-native";
+import { Linking, NativeModules } from "react-native";
 
 import { NavigationProp } from "@react-navigation/native";
 
 interface props {
   navigation: NavigationProp<any, any>;
+  deepLink?: string;
 }
-export const HelloWorldScreen = ({ navigation }: props) => {
+export const HelloWorldScreen = ({
+  navigation,
+  deepLink = "cma00519://Transfers",
+}: props) => {
   const { NativeRouterModule } = NativeModules;
 
   const onPress = () => {
     NativeRouterModule.dismiss();
   };
+
+  function openDeepLink(deepLink: string): void {
+    Linking.canOpenURL(deepLink)
+      .then((supported) => {
+        if (supported) {
+          onPress();
+          Linking.openURL(deepLink);
+        } else {
+          onPress();
+        }
+      })
+      .catch((error) => {
+        onPress();
+        console.error("Error occurred while opening deep link: ", error);
+      });
+  }
 
   return (
     <View
@@ -56,6 +76,39 @@ export const HelloWorldScreen = ({ navigation }: props) => {
         <HStack>
           <Text color="white" fontSize="20" fontWeight={"bold"} ml="0.5">
             WebView Screen
+          </Text>
+          <Text color="white" fontSize="20" mt="0.5" ml="1" fontWeight={"bold"}>
+            →
+          </Text>
+        </HStack>
+      </Button>
+      <Button
+        onPress={() => openDeepLink(deepLink)}
+        _pressed={{ bg: "#b1e004" }}
+        bg="#90b800"
+        mx="6"
+        my="4"
+      >
+        <HStack>
+          <Text color="white" fontSize="20" fontWeight={"bold"} ml="0.5">
+            Transfers Deep Link
+          </Text>
+          <Text color="white" fontSize="20" mt="0.5" ml="1" fontWeight={"bold"}>
+            →
+          </Text>
+        </HStack>
+      </Button>
+
+      <Button
+        onPress={() => openDeepLink("cma00519://InvalidLink")}
+        _pressed={{ bg: "#b1e004" }}
+        bg="#90b800"
+        mx="6"
+        my="4"
+      >
+        <HStack>
+          <Text color="white" fontSize="20" fontWeight={"bold"} ml="0.5">
+            Invalid Deep Link
           </Text>
           <Text color="white" fontSize="20" mt="0.5" ml="1" fontWeight={"bold"}>
             →
